71156f8f2bc02b7bb134b71dec6e1bb188a7e6f6
Karol Przybycin <przybycinkarol@gmail.com>
2025-06-09 20:20:03 +0200

    Excersise number 6

---
+++ Draw.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+def draw_image(x,y):
+    z = np.random.randint(0,255,(x,y,3)).astype('uint8')
+    plt.imshow(z)
+    plt.show()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ImageProcessor
+import Draw
 import os
 
 input_images = []
@@ -17,6 +18,9 @@ images.max_filter_convert(size,mode) # 3 zadanie część 2
 images.median_filter_convert(size,mode) # 4 zadanie
 images.prewitt_convert(mode) # 5 zadanie 1 część
 images.sobel_convert(mode) # 5 zadanie 2 część
+Draw.draw_image(300,400) #6 zadanie
+
+
 
 
 
